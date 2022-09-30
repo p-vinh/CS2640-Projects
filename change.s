4bfadb7144ba71b2abca5a2ea1d179f7390d6771
random-debug <85326677+random-debug@users.noreply.github.com>
2022-09-30 00:02:10 -0700

    Initial commit

---
+++ change.s
@@ -0,0 +1,53 @@
+# 
+#  Name:        Pham, Vinh 
+#  Project:     1 
+#  Due:         October 5, 2022 
+#  Course:      cs-2640-04-f22 
+# 
+#  Description: 
+#               Gives the amount of change you need with the amount given 
+
+        .data
+change:
+        .ascii  "Change by V. Pham\n\n"
+        .asciiz  "Enter the change? "
+        
+        .data
+quarter:  
+        .asciiz "Quarter: "
+
+        .data
+nickel:
+        .asciiz "Nickel: "
+
+
+        .text
+main:   
+        la      $a0, change         # display to ask for change
+        li      $v0, 4
+        syscall
+
+        li      $v0, 5              # read input number
+        syscall
+        move    $a0, $v0            # Moves input number into $a0
+        
+        li      $t0, 25             # Loads 25 into $t0
+        div		$a0, $t0			# $a0 / $t0
+        mflo	$t1					# $t1 = floor($a0 / $t0) How much Quarters
+        mfhi	$t2					# $t2 = $a0 mod $t0 Total Left
+        
+        la      $a0, quarter        # Printing out the string "Quarter: "
+        li      $v0, 4
+        syscall
+        move    $a0, $t1            # Printing out the number needed
+        li      $v0, 1
+        syscall
+        
+        li      $t0, 10             # Loads immediate 10 into $t0
+        div     $t2, $t0
+        mflo	$t1					
+        mfhi	$t2
+
+
+        li      $v0, 10             # exit
+        syscall
