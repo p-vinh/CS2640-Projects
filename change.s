fdadcb7a9c517ba69db7e6eee3d4c464642c8411
random-debug <85326677+random-debug@users.noreply.github.com>
2022-09-30 09:57:04 -0700

    Update change.s

--- change.s
+++ change.s
@@ -9,8 +9,8 @@
 
         .data
 change:
-        .ascii  "Change by V. Pham\n\n"
-        .asciiz  "Enter the change? "
+        .ascii  "\nChange by V. Pham\n\n"
+        .asciiz  "\nEnter the change? "
         
         .data
 quarterText:  
@@ -40,38 +40,27 @@ main:
         syscall
         move    $a0, $v0            # Moves input number into $a0
         
-        li      $t0, 25             # Loads 25 into $t0
-        div		$a0, $t0			# $a0 / $t0
-        mflo	$t1					# $t1 = floor($a0 / $t0) How much Quarters
-        mfhi	$t2					# $t2 = $a0 mod $t0 Total Left
-        
-        bgtz    $t1, quarter
-        
-        li      $t0, 10             # Loads immediate 10 into $t0
-        div     $t2, $t0            # $t2 / $t0
-        mflo	$t1					# $t1 = floor($t2 / $t0) How much Dime
-        mfhi	$t2                 # $t2 = $t2 mod $t0 Total Left
-
-        bgtz    $t1, dime
-
-        li      $t0, 5              # Loads immediate 5 into $t0
-        div     $t2, $t0            # $t2 / $t0
-        mflo	$t1					# $t1 = floor($t2 / $t0) How much Nickel
-        mfhi	$t2                 # $t2 = $t2 mod $t0 Total Left
-
-        li      $t0, 1              # Loads immediate 1 into $t0
-        div     $t2, $t0            # $t2 / $t0
-        mflo	$t1					# $t1 = floor($t2 / $t0) How much Penny
-        mfhi	$t2                 # $t2 = $t2 mod $t0 Total Left
-
-        bgtz    $t1, penny
+        bgtz    $a0, quarter
 
         li      $v0, 10             # exit
         syscall
 
-
         .text
 quarter:
+        li      $t0, 25             # Loads 25 into $t0
+        div		$a0, $t0			# $a0 / $t0
+        mflo	$t1					# $t1 = floor($a0 / $t0) How much Quarters
+        mfhi	$t2					# $t2 = $a0 mod $t0 Total Left
+        bgtz    $t2, dime
+    
+
+
+        
+        li      $v0, 10             # exit
+        syscall
+
+        .text
+dime:
         la      $a0, quarterText    # Printing out the string "Quarter: "
         li      $v0, 4
         syscall
@@ -79,8 +68,18 @@ quarter:
         li      $v0, 1
         syscall
 
+        li      $t0, 10             # Loads immediate 10 into $t0
+        div     $t2, $t0            # $t2 / $t0
+        mflo	$t1					# $t1 = floor($t2 / $t0) How much Dime
+        mfhi	$t2                 # $t2 = $t2 mod $t0 Total Left
+        bgtz    $t2, nickel
+
+
+        li      $v0, 10             # exit
+        syscall
+
         .text
-dime:
+nickel:
         la      $a0, dimeText       # Printing out the string "Dime: "
         li      $v0, 4
         syscall
@@ -88,20 +87,38 @@ dime:
         li      $v0, 1
         syscall
 
+        li      $t0, 5              # Loads immediate 5 into $t0
+        div     $t2, $t0            # $t2 / $t0
+        mflo	$t1					# $t1 = floor($t2 / $t0) How much Nickel
+        mfhi	$t2                 # $t2 = $t2 mod $t0 Total Left
+        bgtz    $t2, penny
+
+
+
+        li      $v0, 10             # exit
+        syscall
+
+
         .text
-nickel:
-        la      $a0, nickelText         # Printing out the string "Nickel: "
+penny:
+        la      $a0, nickelText     # Printing out the string "Nickel: "
+        li      $v0, 4
+        syscall
+        move    $a0, $t1            # Printing out the number needed
+        li      $v0, 1
+        syscall
+
+        li      $t0, 1              # Loads immediate 1 into $t0
+        div     $t2, $t0            # $t2 / $t0
+        mflo	$t1					# $t1 = floor($t2 / $t0) How much Penny
+        mfhi	$t2                 # $t2 = $t2 mod $t0 Total Left
+
+        la      $a0, pennyText      # Printing out the string "Penny: "
         li      $v0, 4
         syscall
         move    $a0, $t1            # Printing out the number needed
         li      $v0, 1
         syscall
         
-        .text
-penny:
-        la      $a0, pennyText          # Printing out the string "Penny: "
-        li      $v0, 4
-        syscall
-        move    $a0, $t1            # Printing out the number needed
-        li      $v0, 1
+        li      $v0, 10             # exit
         syscall
