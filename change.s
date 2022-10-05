6673421ab4b148b896ad202c3547a7ae0b982851
random-debug <85326677+random-debug@users.noreply.github.com>
2022-10-04 17:02:27 -0700

    Whitespaces

--- change.s
+++ change.s
@@ -18,78 +18,77 @@ newline:	.asciiz "\n"
 
 	.text
 main:
-	la	$a0,	change		# display to ask for change
-	li	$v0,	4
+	la	$a0, change		# display to ask for change
+	li	$v0, 4
 	syscall
 
-	li	$v0,	5		# read input number
+	li	$v0, 5		# read input number
 	syscall
-	move	$a0,	$v0		# Moves input number into $a0
-
-	li	$t0,	25		# Loads 25 into $t0
-	div	$a0,	$t0		# $a0 / $t0
-	mflo	$t1			# $t1 = floor($a0 / $t0) How much Quarters
-	mfhi	$t2			# $t2 = $a0 mod $t0 Total Left
-        
-	beqz	$t1,	endif
-	la	$a0,	quarter		# Printing out the string "Quarter: "
-	li	$v0,	4
+	
+	li	$t0, 25		# Loads 25 into $t0
+	div	$v0, $t0		# $a0 / $t0
+	mflo	$t1		# $t1 = floor($a0 / $t0) How much Quarters
+	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+	
+	beqz	$t1, endif
+	la	$a0, quarter	# Printing out the string "Quarter: "
+	li	$v0, 4
 	syscall
-	move	$a0,	$t1		# Printing out the number needed
-	li	$v0,	1
+	move	$a0, $t1		# Printing out the number needed
+	li	$v0, 1
 	syscall
-	li	$a0,	'\n'
-	li	$v0,	11
+	li	$a0, '\n'
+	li	$v0, 11
 	syscall
 endif:
-	li	$t0,	10		# Loads 10 into $t0
-	div	$t2,	$t0		# $a0 / $t0
-	mflo	$t1			# $t1 = floor($a0 / $t0) How much Dime
-	mfhi	$t2			# $t2 = $a0 mod $t0 Total Left
-        
-	beqz	$t1,	endif2
-	la	$a0,	dime		# Printing out the string "Dime: "
-	li	$v0,	4
+	li	$t0, 10		# Loads 10 into $t0
+	div	$t2, $t0		# $a0 / $t0
+	mflo	$t1		# $t1 = floor($a0 / $t0) How much Dime
+	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+
+	beqz	$t1, endif2
+	la	$a0, dime		# Printing out the string "Dime: "
+	li	$v0, 4
 	syscall
-	move	$a0,	$t1		# Printing out the number needed
-	li	$v0,	1
+	move	$a0, $t1		# Printing out the number needed
+	li	$v0, 1
 	syscall
-	li	$a0,	'\n'
-	li	$v0,	11
+	li	$a0, '\n'
+	li	$v0, 11
 	syscall
 endif2:
 
-	li	$t0,	5		# Loads 5 into $t0
-	div	$t2,	$t0		# $a0 / $t0
-	mflo	$t1			# $t1 = floor($a0 / $t0) How much Nickel
-	mfhi	$t2			# $t2 = $a0 mod $t0 Total Left
-        
-	beqz	$t1,	endif3
-	la	$a0,	nickel		# Printing out the string "Nickel: "
-	li	$v0,	4
+	li	$t0, 5		# Loads 5 into $t0
+	div	$t2, $t0		# $a0 / $t0
+	mflo	$t1		# $t1 = floor($a0 / $t0) How much Nickel
+	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+
+	beqz	$t1, endif3
+	la	$a0, nickel	# Printing out the string "Nickel: "
+	li	$v0, 4
 	syscall
-	move	$a0,	$t1		# Printing out the number needed
-	li	$v0,	1
+	move	$a0, $t1		# Printing out the number needed
+	li	$v0, 1
 	syscall
-	li	$a0,	'\n'
-	li	$v0,	11
+	li	$a0, '\n'
+	li	$v0, 11
 	syscall
 endif3:
-	li	$t0,	1		# Loads 1 into $t0
-	div	$t2,	$t0		# $a0 / $t0
-	mflo	$t1			# $t1 = floor($a0 / $t0) How much Pennies
-	mfhi	$t2			# $t2 = $a0 mod $t0 Total Left
-        
-	beqz	$t1,	endif4
-	la	$a0,	penny		# Printing out the string "Penny: "
-	li	$v0,	4
+	li	$t0, 1		# Loads 1 into $t0
+	div	$t2, $t0		# $a0 / $t0
+	mflo	$t1		# $t1 = floor($a0 / $t0) How much Pennies
+	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+
+	beqz	$t1, endif4
+	la	$a0, penny	# Printing out the string "Penny: "
+	li	$v0, 4
 	syscall
-	move	$a0,	$t1		# Printing out the number needed
-	li	$v0,	1
+	move	$a0, $t1		# Printing out the number needed
+	li	$v0, 1
 	syscall
-	li	$a0,	'\n'
-	li	$v0,	11
+	li	$a0, '\n'
+	li	$v0, 11
 	syscall
 endif4:
-	li	$v0,	10                 # exit
+	li	$v0, 10		# exit
 	syscall
