92527b69247d9fad081eaa9f110ea6a580af5b3f
random-debug <85326677+random-debug@users.noreply.github.com>
2022-10-05 21:38:10 -0700

    Update change.s

--- change.s
+++ change.s
@@ -1,11 +1,11 @@
 # 
 #  Name:        Pham, Vinh 
 #  Project:     1 
-#  Due:         October 5, 2022 
+#  Due:         October 6, 2022 
 #  Course:      cs-2640-04-f22 
 # 
 #  Description: 
-#               Gives the amount of change you need with the amount given 
+#               Gives the amount of change you need with the amount given as an input
 
 	.data
 change:	.ascii  "Change by V. Pham\n\n"
@@ -14,21 +14,21 @@ quarter:	.asciiz "Quarter: "
 dime:	.asciiz "Dime: "
 nickel:	.asciiz "Nickel: "
 penny:	.asciiz "Penny: "
-newline:	.asciiz "\n"
+
 
 	.text
 main:
-	la	$a0, change		# display to ask for change
+	la	$a0, change	# display to ask for change
 	li	$v0, 4
 	syscall
 
 	li	$v0, 5		# read input number
 	syscall
-	
+
 	li	$t0, 25		# Loads 25 into $t0
-	div	$v0, $t0		# $a0 / $t0
-	mflo	$t1		# $t1 = floor($a0 / $t0) How much Quarters
-	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+	div	$v0, $t0		
+	mflo	$t1		# How much Quarters
+	mfhi	$t2		# Total Left
 	
 	beqz	$t1, endif
 	la	$a0, quarter	# Printing out the string "Quarter: "
@@ -42,9 +42,9 @@ main:
 	syscall
 endif:
 	li	$t0, 10		# Loads 10 into $t0
-	div	$t2, $t0		# $a0 / $t0
-	mflo	$t1		# $t1 = floor($a0 / $t0) How much Dime
-	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+	div	$t2, $t0		
+	mflo	$t1		# How much Dime
+	mfhi	$t2		# Total Left
 
 	beqz	$t1, endif2
 	la	$a0, dime		# Printing out the string "Dime: "
@@ -57,11 +57,10 @@ endif:
 	li	$v0, 11
 	syscall
 endif2:
-
 	li	$t0, 5		# Loads 5 into $t0
-	div	$t2, $t0		# $a0 / $t0
-	mflo	$t1		# $t1 = floor($a0 / $t0) How much Nickel
-	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+	div	$t2, $t0		
+	mflo	$t1		# How much Nickel
+	mfhi	$t2		# Total Left
 
 	beqz	$t1, endif3
 	la	$a0, nickel	# Printing out the string "Nickel: "
@@ -75,9 +74,9 @@ endif2:
 	syscall
 endif3:
 	li	$t0, 1		# Loads 1 into $t0
-	div	$t2, $t0		# $a0 / $t0
-	mflo	$t1		# $t1 = floor($a0 / $t0) How much Pennies
-	mfhi	$t2		# $t2 = $a0 mod $t0 Total Left
+	div	$t2, $t0		
+	mflo	$t1		# How much Pennies
+	mfhi	$t2		# Total Left
 
 	beqz	$t1, endif4
 	la	$a0, penny	# Printing out the string "Penny: "
